8cebdfca8179bedbcbfcede0a32188fcd305e5f2
YeduKrishnan A <kuttuzz17@gmail.com>
2022-11-24 12:45:02 +0530

    Logic To press a single cell once

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// click sign x 
-// click again change sign to o
-
-
-const eachPiece = document.querySelectorAll(".eachPiece");
+const cell = document.querySelectorAll(".cell");
 
 const GameBoard = {
     gameBoard: ["o", "x"],
     swapElement(array) { // to swap the value using destructor
         [this.gameBoard[0], this.gameBoard[1]] = [this.gameBoard[1], this.gameBoard[0]];
-        console.log(`Is the function working :${GameBoard.gameBoard}`);
+        console.log(` SwapElements : ${GameBoard.gameBoard}`);
     }
 }
 
@@ -16,23 +12,60 @@ const createPlayer = function (name) {
     return { name };
 }
 
-const playerOne = createPlayer("PlayerOne");
-const playerTwo = createPlayer("PlayerTwo");
-
+let playerOne = createPlayer("PlayerOne");
+let playerTwo = createPlayer("PlayerTwo");
 
 let signArray = GameBoard.gameBoard;
-for (let eachPieces of eachPiece) {
-    eachPieces.addEventListener("click", e => {
-        GameBoard.swapElement(signArray);
-        eachPieces.textContent = signArray[0];
-        // playerOne.textContent = signArray;
-        // console.log(playerOne);
-        // console.log(signArray);
-    })
+let isClicked = false; // only can press a SinglePiece
+let isSwitch = false; // to switch player
+
+for (let cells of cell) {
+    if (!isClicked) {
+        isClicked = true;
+        cells.addEventListener("click", e => {
+            if (!isSwitch) {
+                isSwitch = true
+                GameBoard.swapElement(signArray);
+                cells.textContent = signArray[0];
+                playerOne.sign = cells.textContent;
+                console.log("First Player");
+                console.log("------------------------------------------------------");
+            }
+        })
+    }
+    else {
+        isClicked = false;
+        console.log
+        cells.addEventListener("click", e => {
+            if (isSwitch) {
+                isSwitch = false
+                GameBoard.swapElement(signArray);
+                cells.textContent = signArray[0];
+                playerTwo.sign = cells.textContent;
+                console.log("Second Player");
+                console.log("------------------------------------------------------");
+            }
+        })
+    }
 }
+// if (!isClicked) {
+//     isClicked = true;
+// }
+// else {
+//     isClicked = false;
+//     console.log
+// }
+
+
 
 function switchPlayer() {
-    //     if (playerTwo) {
+
+}
+
+//Player Swap Using Destructing
+
+// const gameBoard = ["x", "o", "x", "o"];
+  //     if (playerTwo) {
     //         // switchPlayer()
     //         eachPieces.textContent = sign[1];
     //         playerTwo.textContent = sign.textContent;
@@ -44,9 +77,35 @@ function switchPlayer() {
     //         playerOne.textContent = sign.textContent;
     //         console.log(playerOne);
     //     }
-}
 
 
-// const gameBoard = ["x", "o", "x", "o"];
+// Player Switch
 
+// if (!isClicked) {
+//     isClicked = true;
+// }
+// else {
+//     isClicked = false;
+//     console.log
+// }
 
+// for (let eachPieces of eachPiece) {
+//     eachPieces.addEventListener("click", e => {
+//         if (!isSwitch) {
+//             isSwitch = true
+//             GameBoard.swapElement(signArray);
+//             eachPieces.textContent = signArray[0];
+//             playerOne.sign = eachPieces.textContent;
+//             console.log("First Player");
+//         }
+//         else {
+//             // switchPlayer()
+//             isSwitch = false
+//             GameBoard.swapElement(signArray);
+//             eachPieces.textContent = signArray[0];
+//             playerTwo.sign = eachPieces.textContent;
+//             console.log(playerTwo);
+//             console.log("Second Player");
+//         }
+//     })
+// }
